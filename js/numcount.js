9d4dab960517327e3e8204f89851fd70c912c031
finalprojecticbt <89890530+finalprojecticbt@users.noreply.github.com>
2021-09-06 20:41:29 +0530

    334

---
+++ js/numcount.js
@@ -0,0 +1,63 @@
+
+
+	// Counter
+
+	$('.num-count span').counterUp({
+	      delay: 8,
+	      time: 2000
+	  });
+
+	// Testimonils
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1199,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination: true,
+        autoPlay:true
+    });
+});
+
+
+
+
+    // clients logo slider
+    $('.client-logo-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
